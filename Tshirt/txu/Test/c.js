6f172400c11d088cca5e61cca66b8b3b8cdc1017
hutongtao <jiankehtt@126.com>
2016-06-05 23:39:56 +0800

    1. 研究canvas画图分层 2. 保存canvas画板到图片数据 3. 显示保存的数据（现在保存还有问题，显示为黑板，可能由于图片使用回调所致）

--- Tshirt/txu/Test/c.js
+++ Tshirt/txu/Test/c.js
@@ -1,5 +1,26 @@
+
 function drawImage() {
-	var can = document.getElementById("c1").getContext('2d');
-	can.fillStyle = "#FF00FF";
-	can.fillRect(0, 0, 150, 75);
+	var canvas= document.getElementById("c1")
+	var context=  canvas.getContext('2d');
+// 	var img = $("#img")[0];
+// 	can.fillStyle = 'rgb(0,0,255)';
+// 	can.fillRect(0,0,100,100);
+// 	can.drawImage(img, 0, 100);
+	var image = new Image();
+	image.src = "t1.png";
+	image.onload = function () {
+		context.drawImage(image,0,0);
+		context.fillStyle='rgb(0,0,255)';
+		context.moveTo(10, 10);
+		context.lineTo(150, 50);
+		context.lineTo(10, 50);
+		context.stroke();
+	}
+	
+	console.log(canvas.toDataURL("image/png"));
+	w=window.open(canvas.toDataURL("image/jpeg"),"smallwin","width=400,height=350");
+	
+
+	//将canvas的图片保存base64的数据
+	// $("#img2").attr('src',)
 }
