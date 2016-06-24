a140882dd357fadfa0aa17dd39f328b07ff0906a
peter <peter@192.168.10.111>
2016-06-25 04:56:42 +0800

    微信认证接口

---
+++ Tshirt/txu/weixin/check/check.php
@@ -0,0 +1,17 @@
+<?php
+
+      $timestamp = $_GET['timestamp'];
+      $nonce     = $_GET['nonce'];
+      $token     = $_GET['token'];
+      $signature = $_GET['signature'];
+      $array     = array( $timestamp,$nonce,$token );
+      sort( $array );
+      
+      $tmpstr = implode('', $array);
+      $tmpstr = sha1( $tmpstr );
+      
+      if( $tmpstr == $signature ){
+          echo $_GET['echostr'];
+          exit;
+      }
+      
